be86cd997e4c2f7a8e074d1e42c23df99f13a6c9
Vivek kumar <viveka9415@gmail.com>
2025-07-20 12:41:58 +0530

    add atlus

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const connectDB = ()=>{ mongoose.connect("mongodb://127.0.0.1:27017/Portfolio")
-  .then(()=>{
-    console.log("db is connected");
-  }).catch((err)=>{
-    console.log(err)
-  });
-  
-}
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+      console.log("db is connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export default connectDB;
